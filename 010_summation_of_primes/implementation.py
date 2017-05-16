8ab6ccd1a5efe3b54c6a4ede08fa3e4469f01059
Sotirios Roussis <xtonousou@gmail.com>
2017-05-16 20:18:29 +0300

    2 million sum primes

---
+++ 010_summation_of_primes/implementation.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python2
+
+
+from math import sqrt
+from itertools import count, islice
+
+
+def IsPrime(n):
+    return n > 1 and all(n % i for i in islice(count(2), int(sqrt(n) - 1)))
+
+
+def FindSumOfPrimesUnder(limit):
+    s = 0
+    for num in xrange(3, limit):
+        if IsPrime(num):
+            s += num
+    return s
+
+
+print FindSumOfPrimesUnder(2000000)
